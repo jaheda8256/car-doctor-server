bf3f1a1dbd6b0a4c0cfea1190f71ccd21fc4f552
jaheda8256 <jahedasultana47@gmail.com>
2024-05-05 13:17:30 +0600

    create updateBooking

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const bookingCollection = client.db('carDoctor').collection('bookings');
   const query = {_id: new ObjectId(id)}
 
   const options = {
-  projection: {title: 1, price: 1, service_id: 1},
+  projection: {title: 1, price: 1, service_id: 1, img: 1},
   }; 
 
   const result = await serviceCollection.findOne(query, options);
@@ -51,12 +51,47 @@ const bookingCollection = client.db('carDoctor').collection('bookings');
  });
 
 // booking
+app.get('/bookings', async(req, res) =>{
+  console.log(req.query.email);
+  let query = {};
+  if(req.query?.email){
+    query = {email: req.query.email}
+  }
+  const result = await bookingCollection.find(query).toArray();
+  res.send(result);
+})
+
 app.post('/bookings', async(req,res)=>{
   const booking = req.body;
   console.log(booking);
   const result = await bookingCollection.insertOne(booking);
   res.send(result);
+});
+ 
+app.patch('/bookings/:id' , async(req, res) =>{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+  const updatedBooking = req.body;
+  console.log(updatedBooking);
+  const updateDoc = {
+    $set: {
+      status: updatedBooking.status
+    },
+  };
+
+  const result = await bookingCollection.updateOne(filter, updateDoc);
+  res.send(result);
 })
+
+
+app.delete('/bookings/:id', async(req, res) =>{
+const id = req.params.id;
+const query = {_id: new ObjectId(id)}
+const result = await bookingCollection.deleteOne(query);
+res.send(result);
+
+
+}) 
 
     // Send a ping to confirm a successful connection
     console.log(
